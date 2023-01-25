776b1a3639f3c6fc4fba2b054a7e08eaeb6e8da1
igor <igor.pravdin163@gmail.com>
2023-01-25 20:09:29 +0400

    stepik 4.4.12 https://stepik.org/lesson/623073/step/12?auth=login&unit=618703

--- stepik 4.4.12.py
+++ stepik 4.4.12.py
@@ -1,20 +1,22 @@
-# https://stepik.org/lesson/623073/step/11?auth=login&unit=618703
+# https://stepik.org/lesson/623073/step/12?auth=login&unit=618703
 import json
 import csv
 
 with open('students.json', encoding='utf-8') as file:
     row = json.load(file)
     arr = []
-    shapka = ["name", "phone"]
     for i in row:
-        x={}
-        if i["age"] >=18 and i["progress"]>=75:
-            x[i["name"]]=i["phone"]
+        x = {}
+        if i["age"] >= 18 and i["progress"] >= 75:
+            x["name"] = i["name"]
+            x["phone"] = i["phone"]
             arr.append(x)
+    columns = ["name", "phone"]
+
 
 with open('data.csv', 'w', encoding='utf-8') as file2:
-    row = csv.writer(file2)
-    row.writerow(shapka)
-    for key, value in x.items():
-        row.writerow(key, value)
+    row = csv.DictWriter(file2, fieldnames=columns)
+    row.writeheader()
+    for i in sorted(arr, key = lambda x: x["name"]):
+        row.writerow(i)
 
